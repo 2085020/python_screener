7b1a7d536c16b803ba832bc14e41d9dad2cdf793
cruznavj <joan.cruz@roche.com>
2022-06-13 11:47:25 +0200

    Fixes Orioleando

--- screener.py
+++ screener.py
@@ -12,11 +12,12 @@ from yahoo_fin import stock_info as si
 from pandas import ExcelWriter
 import yfinance as yf
 import pandas as pd
-import datetime
+from datetime import date, datetime
 import time
 import os
 import glob
 import math
+import datetime
 from functions import *
 
 import mysql.connector
@@ -63,6 +64,11 @@ f.write("COLUMN,0\n")
 f.write("HED,Near High WhatchList\n")
 f.close()
 
+f = open("watchlist_python_near_52_sales", "w")
+f.write("COLUMN,0\n")
+f.write("HED,Near High Earnings WhatchList\n")
+f.close()
+
 f = open("watchlist_python_big_volume", "w")
 f.write("COLUMN,0\n")
 f.write("HED,Big Volume WhatchList\n")
@@ -93,6 +99,16 @@ f.write("COLUMN,0\n")
 f.write("HED,Bear Eng WhatchList\n")
 f.close()
 
+f = open("watchlist_python_minervini_large", "w")
+f.write("COLUMN,0\n")
+f.write("HED,Screen Minervini Large\n")
+f.close()
+
+f = open("watchlist_python_long_bases", "w")
+f.write("COLUMN,0\n")
+f.write("HED,Screen Long bases\n")
+f.close()
+
 
 yf.pdr_override()
 
@@ -108,6 +124,8 @@ df_stocks = df_stocks[df_stocks.MarketCap >= 400000000]
 df_stocks = df_stocks[df_stocks.Volume > 200000]
 myvalue = df_stocks[df_stocks.Symbol == 'AAPL']
 tickers = df_stocks['Symbol']
+#tickers = [item.replace(".", "-") for item in df_stocks] # Yahoo Finance uses dashes instead of dots
+
 """
 tickers = ['A', 'AAL', 'AAPL', 'SLB', 'CTVA', 'BMY', 'XOM']
 tickers = ['BMY']
@@ -118,7 +136,13 @@ start_date = datetime.datetime.now() - datetime.timedelta(days=365)
 end_date = datetime.date.today()
 exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
 exportList2 = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
+exportList3 = pd.DataFrame(columns=["Stock", "Industry", "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
+exportList_minervini = exportList3
+exportList_longbases = exportList3
 returns_multiples = []
+
+# List of Industries associated to each Ticker
+industries = df_stocks['Industry']
 
 # Index Returns
 # ALL INDEXES
@@ -133,8 +157,14 @@ index_df = index_nya + index_ixic + index_sp
 index_df['Percent Change'] = index_df['Adj Close'].pct_change()
 index_return = (index_df['Percent Change'] + 1).cumprod()[-1]
 
+
+# List of Tickers & Industry
+df_tickers_list = pd.DataFrame(list(zip(tickers, industries)), columns=['Ticker', 'Industry'])
+df_tickers_final = df_tickers_list.reset_index()
 # Find top 30% performing stocks (relative to the S&P 500)
-for ticker in tickers:
+#for ticker in tickers:
+for index, row in df_tickers_final.iterrows():
+    ticker = row['Ticker'] 
     # Download historical data as CSV for each stock (makes the process faster)
     try: 
         print(f'starting with ticker: {ticker}')
@@ -172,14 +202,16 @@ for ticker in tickers:
 valueRating = 0.8
 #Creating dataframe of only top 30%
 valueRating = 0.7
-rs_df = pd.DataFrame(list(zip(tickers, returns_multiples)), columns=['Ticker', 'Returns_multiple'])
+rs_df = pd.DataFrame(list(zip(df_tickers_list['Ticker'], df_tickers_list['Industry'], returns_multiples)), columns=['Ticker', 'Industry', 'Returns_multiple'])
 rs_df['RS_Rating'] = rs_df.Returns_multiple.rank(pct=True) * 100
-rs_df = rs_df[rs_df.RS_Rating >= rs_df.RS_Rating.quantile(valueRating)]
+rs_df_final = rs_df[rs_df.RS_Rating >= rs_df.RS_Rating.quantile(valueRating)]
 
 # Checking Minervini conditions of top 30% of stocks in given list
-rs_stocks = rs_df['Ticker']
-for stock in rs_stocks:    
+#rs_stocks = rs_df['Ticker']
+#for stock in rs_stocks:    
+for index, row in rs_df_final.iterrows():   
     try:
+        stock = row["Ticker"]
         df = pd.read_csv(f'{stock}.csv', index_col=0)
         sma = [5, 10, 20, 30, 50, 150, 200]
         for x in sma:
@@ -187,6 +219,7 @@ for stock in rs_stocks:
         
         df["AVGVOL"] = round(df['Volume'].rolling(window=50).mean(), 2)
         # Storing required values 
+        industry = row['Industry']
         currentClose = df["Adj Close"][-1]
         lastDayVolume = df["Volume"][-1]
         moving_average_50 = df["SMA_50"][-1]
@@ -196,11 +229,16 @@ for stock in rs_stocks:
         moving_average_10 = df["SMA_10"][-1]
         moving_average_30 = df["SMA_30"][-1]
         moving_average_20 = df["SMA_20"][-1]
+        
         avg_vol = df["AVGVOL"][-1]
         low_of_52week = round(min(df["Low"][-260:]), 2)
         high_of_52week = round(max(df["High"][-260:]), 2)
         low_of_21day = round(min(df["Low"][-21:-4]), 2)
         high_of_21day = round(max(df["High"][-21:-4]), 2)
+        low_of_10day = round(min(df["Low"][-10:]), 2)
+        high_of_10day = round(max(df["High"][-10:]), 2)
+        low_of_12day = round(min(df["Low"][-12:]), 2)
+        high_of_12day = round(max(df["High"][-12:]), 2)
         low_of_3day = round(min(df["Low"][-3:]), 2)
         high_of_3day = round(max(df["High"][-3:]), 2)
 
@@ -217,13 +255,11 @@ for stock in rs_stocks:
         stDev = df["Adj Close"][-21:].std()
 
 
+        RS_Rating = round(rs_df_final[rs_df_final['Ticker']==row['Ticker']].RS_Rating.tolist()[0])
 
         mycursor.execute("""
             Update stock_data2 set `Last Sale` = %s, Volume = %s, `% Change` = %s,  `Net Change` = %s where Symbol = %s 
             """, (str(lastClose), str(lastDayVolume), str(percentChange), str(netChange) ,str(stock) ))
-
-
-        RS_Rating = round(rs_df[rs_df['Ticker']==stock].RS_Rating.tolist()[0])
         
         try:
             moving_average_200_20 = df["SMA_200"][-20]
@@ -254,17 +290,29 @@ for stock in rs_stocks:
         # Condition 5: Current Price > 50 SMA
         condition_5 = currentClose > moving_average_50
            
+        # Condition 5: Current Price > 50 SMA
+        condition_5b = currentClose >= moving_average_50*0.9
+
         # Condition 6: Current Price is at least 30% above 52 week low
         condition_6 = currentClose >= (1.3*low_of_52week)
-           
+        
+        # Condition 6: Current Price is at least 15% above 52 week low
+        condition_6b = currentClose >= (1.15*low_of_52week)
+
         # Condition 7: Current Price is within 25% of 52 week high
         condition_7 = currentClose >= (.75*high_of_52week)
         
-        # Condition 8: 21 days volatility < 25%
-        condition_8 = ((high_of_21day-low_of_21day)/high_of_21day)*100 < 25
+        # Condition 8: 21 days volatility < 23%
+        condition_8 = ((high_of_21day-low_of_21day)/high_of_21day)*100 < 23
 
-        # Condition 9: 3 days volatility < 5%
-        condition_9 = ((high_of_3day-low_of_3day)/high_of_3day)*100 < 10
+        # Condition 8b: 12 days price compressed < 23%
+        condition_8b = ((high_of_12day-low_of_12day)/high_of_12day)*100 < 23
+
+        # Condition 9: 3 days price under 9%%
+        condition_9 = ((high_of_3day-low_of_3day)/high_of_3day)*100 < 9
+
+        # Condition 9b: 3 days price under 7%
+        condition_9b = ((high_of_3day-low_of_3day)/high_of_3day)*100 < 7
 
         # Condition 10: más de un 3% hasta maximos de 52 w
         condition_10 = ((high_of_52week - currentClose) > 0) and (((high_of_52week-currentClose)/high_of_52week)*100 > 3)
@@ -293,12 +341,27 @@ for stock in rs_stocks:
         #Condition 14: Bearish engulfing
         Condition_14BeE = high_of_2day < lastOpen and low_of_2day > lastClose
 
+        # Condition 15: máximos anteriores 50 días más tarde
+        # Calculo diferencia de fechas entre maximos
+        date_previous_max = df["High"][-260:-50].idxmax()
+        d0 = date(date.today().year, date.today().month, date.today().day)
+        dt = datetime.datetime.strptime(date_previous_max, '%Y-%m-%d')
+        d1 = date(dt.year, dt.month, dt.day)
+        delta = d0 - d1
+     
+        df2=df.loc[df["High"][-260:-60].idxmax()]
+        previoushigh_60day = df2['High']
+
+        condition_15 = (previoushigh_60day <= 1.30*high_of_52week) and (previoushigh_60day >= 0.85*high_of_52week) 
+        #and (currentClose <= 1.15*previous_50day_high) and (currentClose >= 0.85*previous_50day_high)
+
         # Condition 13: 3% hasta la media de 20
         #condition_13 = (abs(currentClose - moving_average_20)/currentClose)*100 < 3
 
         # Condition 14: 5% hasta la media de 30
         #condition_14 = (abs(currentClose - moving_average_30)/currentClose)*100 < 5
-
+        epsGrowth = 0
+        salesGrowth = 0
         # If all conditions above are true, add stock to exportList
         if(condition_price and condition_0b and condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9 and condition_10 and condition_10b and condition_11):
         #if(condition_0 and condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9 and condition_13):
@@ -323,10 +386,38 @@ for stock in rs_stocks:
                 f.write(f"SYM,{stock},SMART/AMEX,\n")
                 f.close()
 
-        if (condition_price and condition_0b and condition_10b):
-            f = open("watchlist_python_near_52", "a")
+        minervini = condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8b and condition_9 and condition_10
+        longBases = condition_5b and condition_6b and condition_7 and condition_8 and condition_9b and condition_10 and condition_11
+
+        if(minervini):
+            exportList_minervini = exportList_minervini.append({"Stock": row['Ticker'], "Industry": row['Industry'], "RS_Rating": RS_Rating ,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
+            print (row['Ticker'] + " made the Minervini requirements")
+            f = open("watchlist_python_minervini_large", "a")
             f.write(f"SYM,{stock},SMART/AMEX,\n")
             f.close()
+
+        if(not minervini and longBases):
+            exportList_longbases = exportList_longbases.append({'Stock': row['Ticker'], 'Industry': row['Industry'], "RS_Rating": RS_Rating ,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
+            print (row['Ticker'] + " made the Long Bases requirements")
+            f = open("watchlist_python_long_bases", "a")
+            f.write(f"SYM,{stock},SMART/AMEX,\n")
+            f.close()
+
+
+        if (condition_price and condition_0b and condition_10b):
+            if (epsGrowth == 0):
+                epsGrowth = calculateEPS(stock)
+                print("calculate Sales:"+stock)
+                salesGrowth = calculateRevenueGrowth(stock)
+            
+            if (epsGrowth > 10 and salesGrowth > 10):
+                f = open("watchlist_python_near_52_sales", "a")
+                f.write(f"SYM,{stock},SMART/AMEX,\n")
+                f.close()
+            else:
+                f = open("watchlist_python_near_52", "a")
+                f.write(f"SYM,{stock},SMART/AMEX,\n")
+                f.close()
 
         if (condition_price and condition_0 and condition_12):
             f = open("watchlist_python_big_volume", "a")
@@ -375,6 +466,16 @@ writer.save()
 writer2 = ExcelWriter("ScreenOutput2.xlsx")
 exportList2.to_excel(writer2, "Sheet1")
 writer2.save()
+
+
+exportList_minervini = exportList_minervini.sort_values(by='RS_Rating', ascending=False)
+exportList_longbases = exportList_longbases.sort_values(by='RS_Rating', ascending=False)
+print('\n', exportList)
+print('\n', exportList_longbases)
+writer = ExcelWriter("ScreenOutputMinervini.xlsx")
+exportList_minervini.to_excel(writer, "Minervini")
+exportList_longbases.to_excel(writer, "Long_Bases")
+writer.save()
 
 mydb.commit()
 mydb.close()
